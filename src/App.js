8ab1e6b99bf506b78b7a2aa270fb0667a60386e9
raphael <paulberthet94@gmail.com>
2022-07-28 17:20:32 +0200

    intial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://lereacteur-vinted-api.herokuapp.com/offers"
+    );
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <div className='offer'>
+      {data.offers.map((offer, index) => {
+        return (
+          <div key={index}>
+            {offer.owner ? (
+              <div className='offer-avatar'>
+                <img src={offer.owner.account.avatar.secure_url}></img>
+                <h3>{offer.owner.account.username}</h3>
+              </div>
+            ) : (
+              <h3>pas de owner</h3>
+            )}
+
+            {/* {offer.product_pictures.map((item, index) => {
+              return (
+                <div className='offer-img'>
+                  <img src={item.secure_url}></img>
+                </div>
+              );
+            })} */}
+            <img src={offer.product_image.secure_url}></img>
+            <p>{offer.product_price} €</p>
+            {offer.product_details.map((item, index) => {
+              return (
+                <>
+                  <p>{item.MARQUE}</p>
+                  <p>{item.TAILLE}</p>
+                </>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
