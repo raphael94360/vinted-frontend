225aaf231a0daedb46dfb97cc14de6c969120ccb
raphael <paulberthet94@gmail.com>
2022-07-29 09:58:21 +0200

    modification sur offer.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,46 @@
-import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const [data, setData] = useState({})
-  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("https://lereacteur-vinted-api.herokuapp.com/offers")
-      setData(response.data)
-      setIsLoading(false)
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      setData(response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error.response)
+      console.log(error.response);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return isLoading ? (
     <span> En cours de chargement... </span>
   ) : (
-    <div className="offer">
-      {data.offers.map(offer => {
+    <div className='offer'>
+      {data.offers.map((offer) => {
         return (
           <Link to={`/offer/${offer._id}`} key={offer._id}>
-            <div className="link">
+            <div className='link'>
               {offer.owner && (
-                <div className="offer-avatar">
-                  <img src={offer.owner.account.avatar.secure_url} alt="coucou"></img>
+                <div className='offer-avatar'>
+                  <img
+                    src={offer.owner.account.avatar.secure_url}
+                    alt='avatar'
+                  ></img>
                   <h3>{offer.owner.account.username}</h3>
                 </div>
               )}
 
-              <img src={offer.product_image.secure_url} alt="coucou"></img>
+              <img src={offer.product_image.secure_url} alt='product'></img>
               <p>{offer.product_price} €</p>
               {offer.product_details.map((item, index) => {
                 return (
@@ -43,14 +48,14 @@ const Home = () => {
                     <p>{item.TAILLE}</p>
                     <p>{item.MARQUE}</p>
                   </div>
-                )
+                );
               })}
             </div>
           </Link>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,36 +1,55 @@
-import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Offer = () => {
-  const { id } = useParams()
-  const [dataOffer, setDataOffer] = useState({})
-  console.log(dataOffer)
-  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams();
+  const [data, setData] = useState({});
+  // console.log(data);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`https://lereacteur-vinted-api.herokuapp.com/offer/${id}`)
-      setDataOffer(response.dataOffer)
-      setIsLoading(false)
+      const response = await axios.get(
+        `https://lereacteur-vinted-api.herokuapp.com/offer/${id}`
+      );
+      setData(response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error.response)
+      console.log(error.response);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return isLoading ? (
     <span> En cours de chargement... </span>
   ) : (
-    <div className="offer-single">
-      {dataOffer.offers.map(offer => {
-        return <img src={offer.product_image.secure_url}></img>
-      })}
+    <div className='offer-single'>
+      <div>
+        <img src={data.product_image.secure_url} alt='product'></img>
+      </div>
+      <div>
+        <p>{data.product_price} € </p>
+        <ul>
+          {data.product_details.map((detail, index) => {
+            return (
+              <li key={index}>
+                <span>{Object.keys(detail)} : </span>
+                <span>{Object.values(detail)}</span>
+              </li>
+            );
+          })}
+        </ul>
+        <p>{data.product_name}</p>
+        <p>{data.product_description}</p>
+        <img src={data.owner.account.avatar.secure_url} alt='avatar'></img>
+        <p>{data.owner.account.username}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offer
+export default Offer;
